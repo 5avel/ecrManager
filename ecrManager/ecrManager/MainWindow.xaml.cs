d64c5fbccb777300a077d143aba427af990ee81b
Pavel Boliybash <pavel.b@live.ru>
2015-02-11 13:50:25 +0200

    Связ Грида с БД.

--- ecrManager/ecrManager/MainWindow.xaml.cs
+++ ecrManager/ecrManager/MainWindow.xaml.cs
@@ -1,17 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using System.Data;
+using System.Data.SqlClient;
+using ecrmini;
 
 namespace ecrManager
 {
@@ -24,5 +15,47 @@ namespace ecrManager
         {
             InitializeComponent();
         }
+
+         private DataTable _dataTable;
+         private SqlDataAdapter adapter;
+
+    public DataTable DataTable
+    {
+        get { return _dataTable; }
+        set { _dataTable = value; }
+    }
+
+    private void Window_Loaded(object sender, RoutedEventArgs e)
+    {
+
+
+           
+        }
+
+    private void Grid_Loaded(object sender, RoutedEventArgs e)
+    {
+        string cs = @"Data Source=(LocalDB)\v11.0;AttachDbFilename=C:\Users\Pavel\Source\Repos\ecrmanager\ecrManager\ecrManager\Database.mdf;Integrated Security=True";
+
+        SqlConnection conn = new SqlConnection(cs);
+        SqlCommand cmd = new SqlCommand("SELECT * FROM Goods", conn);
+        
+        adapter = new SqlDataAdapter(cmd);
+        SqlCommandBuilder cb = new SqlCommandBuilder(adapter);
+        this._dataTable = new DataTable("Goods");
+        adapter.Fill(this._dataTable);
+        this.grid1.DataContext = this;
+    }
+
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        adapter.Update(this._dataTable);
+
+        //t400 t = new t400();
+        //t.t400me()
+    }
+
+
+
+
     }
 }
